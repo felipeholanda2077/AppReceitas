51a439f6be7edbd0cf5d3a0802eb32fb7fb30f30
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-04-11 18:25:20 -0300

    Adicação de API

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
-
-import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
-
+import { useState, useEffect } from "react";
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, FlatList  } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
 import { Logo } from "../../components/logo";
+import { api } from "../../services/api";
 
 export function Home() {
     const [inputValue, setInputValue] = useState("");
+    const [foods, setFoods] = useState([]);
+
+    useEffect(() => {
+
+     async function fetchApi() {
+        const response = await api.get("/foods");
+        setFoods(response.data);
+      }
+
+      fetchApi();
+
+    }, []);
 
     function hendleSearch() {
-        console.log('Foi digitado:');
+        console.log('Foi digitado:npx expo');
         console.log(inputValue);
     }
 
@@ -33,6 +43,14 @@ export function Home() {
             <Ionicons name="search" size={28} color="#4CBE6C" />
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={foods}
+        keyExtractor={ (item) => String(item.id) }
+        renderItem={ ({ item }) => <Text>{item.name}</Text> }
+      />
+
+
     </SafeAreaView>
   );
 }
