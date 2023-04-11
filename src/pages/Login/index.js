f9185dddb637c63cacbad4ab9d835a9d6edd67b3
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2023-04-11 20:30:00 -0300

    Inicialização do desenvolvimento da Página Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,22 +1,62 @@
-import { View, Text, StyleSheet, TextField } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, TextInput, Button, TouchableOpacity } from "react-native";
 
 export function Login() {
   return (
-    <View style={styles.container}>
-      <TextField
-        label={'Email'}
-        placeholder={'Digite seu email'}
-        onChangeText={text => setValue('email', text)}
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 16 }}>Login</Text>
+
+
+      <TextInput
+        style={styles.input}
+        placeholder="Usuario"
+        value={Text}
       />
-      <TextField
-        label={'Senha'}
-        placeholder={'Digite sua senha'}
-        onChangeText={text => setValue('password', text)}
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        keyboardType="numeric"
       />
-    </View>
+
+      <TouchableOpacity>
+        <Text style={styles.pass}>Cadastre-se</Text>
+      </TouchableOpacity>
+
+      <Button styles={styles.button} color="#06d37d" title="Acessar" onPress={() => { }} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F9FF',
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 50,
+    margin: 12,
+    borderWidth: 0.4,
+    padding: 10,
+    borderRadius: 10,
+  },
+  pass: {
+    color: '#4CBE6C',
+    textAlign: 'right',
+    right: 15,
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  button: {
+    color: '#FFF',
+    borderRadius: 50,
+    width: '50%',
+    height: 40,
+    margin: 12,
+    padding: 10,
+  }
 });
